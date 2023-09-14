357756b24bd1fcf0c92b2ceff42cbad2e3dd4d0b
Noah Filliben (Class of 2025) <250510nf@salesianum.org>
2023-09-14 14:52:52 -0400

    age and name thing

--- gooby.py
+++ gooby.py
@@ -2,3 +2,8 @@ if __name__ == '__main__':
     rad = input("radius of circle: ")
     circle = 3.14 * float(rad) ** 2
     print(circle)
+name = input("enter name: ")
+date = input("enter date of birth: ")
+age = 2023 - int(date)
+print("your name is"); print(name)
+print("you are"); print(str(age)); print("years old")
