b9bd5c9777162e5c5f0c5289bf80111d6a2b78ec
Noah Filliben (Class of 2025) <250510nf@salesianum.org>
2023-09-20 09:51:32 -0400

    dicetime

--- testers.py
+++ testers.py
@@ -1,5 +1,8 @@
 import random
 
-rando = random.random()
-rando *= 256
-print(int(rando))
+#rando = random.random()
+#rando *= 256
+#print(int(rando))
+
+dooby = random.randint(1,(random.randint(8,100)))
+print(dooby)
