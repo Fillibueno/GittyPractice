3b1fa220e7fb50b81b46633f7fea38706f2f6649
Noah Filliben (Class of 2025) <250510nf@salesianum.org>
2023-09-21 13:51:57 -0400

    dice stuff

--- testers.py
+++ testers.py
@@ -1,8 +1,19 @@
-import random
+import random as rand
 
 #rando = random.random()
 #rando *= 256
 #print(int(rando))
 
-dooby = random.randint(1,(random.randint(8,100)))
-print(dooby)
+#dooby = rand.randint(1,(rand.randint(8,100)))
+
+#print(dooby)
+myList = ["agate", "renne", "estelle", "kloe", "joshua", "anelace", "mueller"]
+prob = myList[rand.randint(0, len(myList)-1)]
+print(prob)
+
+def rollDie(sides):
+    
+    return rand.randint(1, sides)
+
+print(rollDie(20))
+print(rollDie(6))
