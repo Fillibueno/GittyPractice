cfa5aaa8dec160252fe0cb9a476a753855bdbbc8
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-09-28 11:32:18 -0400

    JJ

---
+++ drooble.py
@@ -0,0 +1,10 @@
+greet = 'His last name is "Orlando"'
+greet1 = 'She said,\n"Plato"'
+greet2 = '''She enjoys going by the name 
+MacDowel'''
+print(greet2)
+print(greet)
+print(greet1)
+myCollege = "cooper"
+print(myCollege[-2])
+print(len(greet))
