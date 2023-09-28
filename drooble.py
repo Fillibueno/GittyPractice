6c63fafb05b2cefc01a900982400d61c0be5a893
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-09-28 11:49:25 -0400

    J

--- drooble.py
+++ drooble.py
@@ -1,10 +1,23 @@
-greet = 'His last name is "Orlando"'
-greet1 = 'She said,\n"Plato"'
-greet2 = '''She enjoys going by the name 
-MacDowel'''
-print(greet2)
-print(greet)
-print(greet1)
+greetx = 'His last name is "Orlando"'
+greet1x = 'She said,\n"Plato"'
+greet2x = '''She enjoys going by the name 
+MacDowell'''
+print(greet2x)
+print(greetx)
+print(greet1x)
 myCollege = "cooper"
 print(myCollege[-2])
-print(len(greet))
+print(len(greetx))
+
+greet = "Orlando"
+greet1 = "Plato"
+greet2 = "MacDowell"
+greet3 = "Bannings"
+list1 = [greet, greet1, greet2, greet3]
+print(list1)
+Toaster = "Orlando Plato MacDowell Bannings"
+print(Toaster.split("o"))
+characters = ["Bright", "Astray", "Harvey", "Lenheim", "Auslese", "Crosner", "Russel", "Vathek"]
+intro = "these are the last names of the characters: "
+print(intro + ", ".join(characters))
+
