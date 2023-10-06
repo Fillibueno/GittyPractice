d95efd7b3bb3bdbc07835af0916b44632fae9e1a
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-10-06 14:52:16 -0400

    madlibs

--- madlibs.py
+++ madlibs.py
@@ -3,19 +3,43 @@ Never impressed by me acing your tests
 All the girls that you've run dry have tired lifeless eyes
 'Cause you burned them out
 But I took your matches before fire could catch me
-So don't look now
-I'm shining like fireworks over your sad empty town'''
+So don't look now 
+I'm shining like fireworks over your sad empty town
+Dear John, I see it all now that you're gone
+Don't you think I was too young to be messed with?
+The girl in the dress cried the whole way home
+I see it all now that you're gone
+Don't you think I was too young to be messed with?
+The girl in the dress wrote you a song
+You should've known'''
 start = 0
 end = 99
 npass = ""
 npass += passage[start:end]
-npass += input("enter plural noun here")
-print(npass)
+npass += input("enter a plural noun here: ")
 epass = ""
-epass += passage[105:-34]
-epass += input("enter plural bright objects")
-print(epass)
+epass += passage[104:193]
+epass += input("enter a plural flamable object here: ")
 wpass = ""
-wpass += passage[-24:-5]
-wpass += input("enter singular object here")
+wpass += passage[200:242]
+wpass += input("enter either 'now', 'soon', or 'then' here: ")
+dpass = ""
+dpass += passage[245:251]
+dpass += input("enter present tense verb here: ")
+xpass = ""
+xpass += passage[258:264]
+xpass += input("enter noun here: ")
+spass = ""
+spass += passage[274:284]
+spass += input("enter adjective here: ")
+mpass = ""
+mpass += passage[287]
+mpass += input("enter adjective here: ")
+mpass += passage[293:299]
+print(npass)
+print(epass)
 print(wpass)
+print(dpass)
+print(xpass)
+print(spass)
+print(mpass)
