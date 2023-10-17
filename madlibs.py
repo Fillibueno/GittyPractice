684b52949a7b2fb4289d7247f5d794ec37335beb
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-10-17 09:48:50 -0400

    j

--- madlibs.py
+++ madlibs.py
@@ -12,80 +12,86 @@ I see it all now that you're gone
 Don't you think I was too young to be messed with?
 The girl in the dress wrote you a song
 You should've known'''
-jpass = ""
-jpass += passage[0:21]
-jpass += input("enter a word here: ")
-lpass = ""
-lpass += passage[27:39]
-lpass += input("enter a noun here: ")
-qpass = ""
-qpass += passage[45:58]
-qpass += input("enter a verb here: ")
-wpass = ""
-wpass += passage[68:86]
-wpass += input("enter a noun here: ")
-npass = ""
-npass += passage[91:99]
-npass += input("enter a plural noun here: ")
-ppass = ""
-ppass += passage[107:119]
-ppass += input("enter a past tense verb here: ")
-opass = ""
-opass += passage[127:132]
-opass += input("enter an adjective here:")
-upass = ""
-upass += passage[137:147]
-upass += input("enter a body part")
-ypass = ""
-ypass += passage[153:160]
-ypass += input("enter a pronoun here:")
-tpass = ""
-tpass += passage[163:164]
-tpass += input("enter a verb here:")
-epass = ""
-epass += passage[170:197]
-epass += input("enter a plural flamable object here: ")
-rpass = ""
-rpass += passage[204:212]
-rpass += input("enter a hot object here: ")
 apass = ""
-apass += passage[216:223]
-apass += input("enter a verb here: ")
+apass += passage[0:21]
+apass += input("enter a word here: ")
+bpass = ""
+bpass += passage[27:39]
+bpass += input("enter a noun here: ")
 cpass = ""
-cpass += passage[228:242]
+cpass += passage[44:58]
 cpass += input("enter a verb here: ")
-wpass = ""
-wpass += passage[200:242]
-wpass += input("enter either 'now', 'soon', or 'then' here: ")
 dpass = ""
-dpass += passage[245:251]
-dpass += input("enter present tense verb here: ")
-xpass = ""
-xpass += passage[258:264]
-xpass += input("enter noun here: ")
-spass = ""
-spass += passage[274:284]
-spass += input("enter adjective here: ")
+dpass += passage[68:85]
+dpass += input("enter a plural noun here: ")
+epass = ""
+epass += passage[91:99]
+epass += input("enter a plural noun here: ")
+fpass = ""
+fpass += passage[105:117]
+fpass += input("enter a past tense verb here: ")
+gpass = ""
+gpass += passage[125:130]
+gpass += input("enter an adjective here:")
+hpass = ""
+hpass += passage[136:145]
+hpass += input("enter a body part")
+ipass = ""
+ipass += passage[150:157]
+ipass += input("enter a pronoun here:")
+jpass = ""
+jpass += passage[160:161]
+jpass += input("enter a verb here:")
+upass = ""
+upass += passage[168:181]
+upass += input("enter a pronoun here: ")
+kpass = ""
+kpass += passage[183:193]
+kpass += input("enter a plural flamable object here: ")
+lpass = ""
+lpass += passage[201:208]
+lpass += input("enter a hot object here: ")
 mpass = ""
-mpass += passage[287]
-mpass += input("enter adjective here: ")
-mpass += passage[293:299]
-print(jpass)
-print(lpass)
-print(qpass)
-print(wpass)
-print(npass)
-print(ppass)
-print(opass)
-print(upass)
-print(ypass)
-print(tpass)
-print(epass)
-print(rpass)
+mpass += passage[213:219]
+mpass += input("enter a verb here: ")
+npass = ""
+npass += passage[224:242]
+npass += input("enter a verb here: ")
+opass = ""
+opass += passage[200:242]
+opass += input("enter either 'now', 'soon', or 'then' here: ")
+ppass = ""
+ppass += passage[245:251]
+ppass += input("enter present tense verb here: ")
+qpass = ""
+qpass += passage[258:264]
+qpass += input("enter noun here: ")
+rpass = ""
+rpass += passage[274:284]
+rpass += input("enter adjective here: ")
+spass = ""
+spass += passage[287]
+spass += input("enter adjective here: ")
+tpass = ""
+tpass += passage[293:299]
 print(apass)
+print(bpass)
 print(cpass)
-print(wpass)
 print(dpass)
-print(xpass)
-print(spass)
+print(epass)
+print(fpass)
+print(gpass)
+print(hpass)
+print(ipass)
+print(jpass)
+print(upass)
+print(kpass)
+print(lpass)
 print(mpass)
+print(npass)
+print(opass)
+print(ppass)
+print(qpass)
+print(rpass)
+print(spass)
+print(tpass)
