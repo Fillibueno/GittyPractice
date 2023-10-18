ba280da28cbfeaf801455941370a360ebd891be6
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-10-18 13:56:06 -0400

    J

--- madlibs.py
+++ madlibs.py
@@ -4,94 +4,95 @@ All the girls that you've run dry have tired lifeless eyes
 'Cause you burned them out
 But I took your matches before fire could catch me
 So don't look now 
-I'm shining like fireworks over your sad empty town
-Dear John, I see it all now that you're gone
-Don't you think I was too young to be messed with?
-The girl in the dress cried the whole way home
-I see it all now that you're gone
-Don't you think I was too young to be messed with?
-The girl in the dress wrote you a song
-You should've known'''
-apass = ""
-apass += passage[0:21]
-apass += input("enter a word here: ")
-bpass = ""
-bpass += passage[27:39]
-bpass += input("enter a noun here: ")
-cpass = ""
-cpass += passage[44:58]
-cpass += input("enter a verb here: ")
-dpass = ""
-dpass += passage[68:85]
-dpass += input("enter a plural noun here: ")
-epass = ""
-epass += passage[91:99]
-epass += input("enter a plural noun here: ")
-fpass = ""
-fpass += passage[105:117]
-fpass += input("enter a past tense verb here: ")
-gpass = ""
-gpass += passage[125:130]
-gpass += input("enter an adjective here:")
-hpass = ""
-hpass += passage[136:145]
-hpass += input("enter a body part")
-ipass = ""
-ipass += passage[150:157]
-ipass += input("enter a pronoun here:")
-jpass = ""
-jpass += passage[160:161]
-jpass += input("enter a verb here:")
-upass = ""
-upass += passage[168:181]
-upass += input("enter a pronoun here: ")
-kpass = ""
-kpass += passage[183:193]
-kpass += input("enter a plural flamable object here: ")
-lpass = ""
-lpass += passage[201:208]
-lpass += input("enter a hot object here: ")
-mpass = ""
-mpass += passage[213:219]
-mpass += input("enter a verb here: ")
-npass = ""
-npass += passage[224:242]
-npass += input("enter a verb here: ")
-opass = ""
-opass += passage[200:242]
-opass += input("enter either 'now', 'soon', or 'then' here: ")
-ppass = ""
-ppass += passage[245:251]
-ppass += input("enter present tense verb here: ")
-qpass = ""
-qpass += passage[258:264]
-qpass += input("enter noun here: ")
-rpass = ""
-rpass += passage[274:284]
-rpass += input("enter adjective here: ")
-spass = ""
-spass += passage[287]
-spass += input("enter adjective here: ")
-tpass = ""
-tpass += passage[293:299]
-print(apass)
-print(bpass)
-print(cpass)
-print(dpass)
-print(epass)
-print(fpass)
-print(gpass)
-print(hpass)
-print(ipass)
-print(jpass)
-print(upass)
-print(kpass)
-print(lpass)
-print(mpass)
-print(npass)
-print(opass)
-print(ppass)
-print(qpass)
-print(rpass)
-print(spass)
-print(tpass)
+I'm shining like fireworks over your sad empty town'''
+newPassage = ""
+start = passage[0]
+end = passage[21]
+newPassage += passage[start:end]
+newPassage += input("enter a word here: ")
+start = passage[26]
+end = passage[39]
+newPassage += passage[start:end]
+newPassage += input("enter a plural noun here: ")
+start = passage[44]
+end = passage[58]
+newPassage += passage[start:end]
+newPassage += input("enter a verb here: ")
+start = passage[67]
+end = passage[85]
+newPassage += passage[start:end]
+newPassage += input("enter a plural noun here: ")
+start = passage[90]
+end = passage[99]
+newPassage += passage[start:end]
+newPassage += input("enter a plural noun here: ")
+start = passage[104]
+end = passage[117]
+newPassage += passage[start:end]
+newPassage += input("enter a past tense verb here: ")
+start = passage[124]
+end = passage[130]
+newPassage += passage[start:end]
+newPassage += input("enter an adjective here: ")
+start = passage[135]
+end = passage[145]
+newPassage += passage[start:end]
+newPassage += input("enter a body part here: ")
+start = passage[149]
+end = passage[157]
+newPassage += passage[start:end]
+newPassage += input("enter a pronoun here: ")
+start = passage[3]
+end = passage[3]
+newPassage = passage[start:end]
+newPassage += input("enter a verb here: ")
+start = passage[167]
+end = passage[181]
+newPassage += passage[start:end]
+newPassage += input("enter a pronoun here: ")
+start = passage[182]
+end = passage[193]
+newPassage += passage[start:end]
+newPassage += input("enter a plural flamable object here: ")
+start = passage[200]
+end = passage[208]
+newPassage += passage[start:end]
+newPassage += input("enter a hot object here: ")
+start = passage[212]
+end = passage[219]
+newPassage += passage[start:end]
+newPassage += input("enter a verb here: ")
+start = passage[224]
+end = passage[237]
+newPassage += passage[start:end]
+newPassage += input("enter a verb here: ")
+start = passage[3]
+end = passage[3]
+newPassage += passage[start:end]
+newPassage += input("enter either 'now', 'soon', or 'then' here: ")
+start = passage[245]
+end = passage[251]
+newPassage += passage[start:end]
+newPassage += input("enter present tense verb here: ")
+start = passage[258]
+end = passage[264]
+newPassage += passage[start:end]
+newPassage += input("enter noun here: ")
+start = passage[273]
+end = passage[284]
+newPassage += passage[start:end]
+newPassage += input("enter adjective here: ")
+start = passage[293]
+end = passage[293]
+newPassage = passage[start:end]
+newPassage += input("enter adjective here: ")
+start = passage[293]
+end = passage[300]
+newPassage += passage[start:end]
+print(newPassage)
+
+#def madlibsJoiner(passage,prompt,start, end):
+    #return(passage[start:end]+input(prompt))
+
+#nputPrompt = "Please enter"
+ #+= madlibsJoiner(passage, inputPrompt, )
