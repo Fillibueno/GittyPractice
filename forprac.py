8cb92c32b1dfaa1143b06baf99b2730ea38dcc8d
Fillibueno <143013846+Fillibueno@users.noreply.github.com>
2023-11-09 12:48:45 -0500

    J

--- forprac.py
+++ forprac.py
@@ -1,9 +1,10 @@
 def main():
     def bigloop():
         bigList = [["Estelle", "Agate", "Olivier", "Kloe", "Tita", "Joshua", "Scherazard", "Zin"], ["Lloyd", "Randy", "Tio", "Elie", "Wazy", "Rixia", "Noel", "Dudley"], ["Rean", "Emma", "Laura", "Jusis", "Machias", "Gaius", "Elliot", "Alisa", "Fie"]]
-        for list in  bigList:
-            for index in range(len(list)):
-                print(list[index])
+        for charas in range(len(bigList)):
+            for index1 in charas:
+                print(charas[index1])
+    bigloop()
 if __name__ == '__main__':
     main()
             
